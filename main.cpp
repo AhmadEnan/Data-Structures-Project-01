498ab53a69444bda2e1612877cb2418148460b72
hadeeltemraz <hadeel.temraz07@eng-st.cu.edu.eg>
2026-05-01 20:34:32 +0300

    Update main.cpp to include QueueArray display and queue tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "LinkedList.h"
 #include <iostream>
+#include "Queue.h"
 
 int main() {
     // ===== LinkedList Test ========================================
@@ -17,7 +18,15 @@ int main() {
     // ===== StackLinkedList Test ===================================
 
     // ===== QueueArray Test ========================================
-
+    QueueArray queue(5);
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+    std::cout << "Queue after insertions: ";
+    queue.display();
+    queue.dequeue();
+    std::cout << "Queue after dequeuing: ";
+    queue.display();
     // ===== QueueLinkedList Test ===================================
 
     return 0;
